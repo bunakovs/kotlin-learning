b2cc948f14e303b43f51f54a4d2c53c82046a523
Eugene Bunakov <egnjonson@gmail.com>
2020-05-08 19:53:14 +0300

    commit

---
+++ Variables.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>){
+    //val (value) = final in Java
+    val a: Int
+    val b = 2
+    val c: String
+    val d = "Hello"
+    //var (variable)
+    var e = 7
+    var f: Char = 'z'
+}
